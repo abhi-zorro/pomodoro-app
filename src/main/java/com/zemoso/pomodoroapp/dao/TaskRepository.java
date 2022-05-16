d38be3743d401b7063efa5d3d8030eb5c18319f5
abhi-zorro <abhishek.bhemisetty@zemosolabs.com>
2022-05-16 23:15:54 +0530

    removed unused imports to fix codesmells

--- src/main/java/com/zemoso/pomodoroapp/dao/TaskRepository.java
+++ src/main/java/com/zemoso/pomodoroapp/dao/TaskRepository.java
@@ -1,6 +1,5 @@
 package com.zemoso.pomodoroapp.dao;
 
-import com.zemoso.pomodoroapp.entity.Pomodoro;
 import com.zemoso.pomodoroapp.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
