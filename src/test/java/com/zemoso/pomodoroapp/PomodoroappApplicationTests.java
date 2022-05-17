68da205dcb77562c29e58d309e8a78dcc3da3c65
abhi-zorro <abhishek.bhemisetty@zemosolabs.com>
2022-05-17 06:39:28 +0530

    added few unit tests

--- src/test/java/com/zemoso/pomodoroapp/PomodoroappApplicationTests.java
+++ src/test/java/com/zemoso/pomodoroapp/PomodoroappApplicationTests.java
@@ -1,13 +1,34 @@
 package com.zemoso.pomodoroapp;
 
+import com.zemoso.pomodoroapp.controller.DemoController;
+import com.zemoso.pomodoroapp.controller.PomoController;
+import com.zemoso.pomodoroapp.controller.UserController;
+import com.zemoso.pomodoroapp.entity.Pomodoro;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class PomodoroappApplicationTests {
 
+	//testing the controller package
+
+	@Autowired
+	private UserController userController;
+
+	@Autowired
+	private DemoController demoController;
+
+	@Autowired
+	private PomoController pomoController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(userController).isNotNull();
+		assertThat(demoController).isNotNull();
+		assertThat(pomoController).isNotNull();
 	}
 
 }
